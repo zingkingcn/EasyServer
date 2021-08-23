0d3abcf81143ab5d7f39e1a97d10c931359e5750
Z.kai <bridge_passerby@163.com>
2021-08-23 13:55:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### 轻量级 Spring Boot + mybatis + MySql + Mongo + redis + jwt + Shiro + Swagger + 国密加密 Java纯服务接口项目
+#### 主要功能
+- 打包输出 war 包
+- 使用 gradle 依赖第三方库
+- 摒弃 xml 配置
+- MySql 连接业务数据库
+- Mongo 连接日志数据库
+- redis 保存临时参数，例如验证码等
+- jwt 生成token
+- shiro 控制接口访问权限
+- swagger 提供接口文档
+- 国密算法提供加解密参数出入参 
---
+++ src/main/java/cn/zingking/spring/easyserver/EasyServerApplication.java
@@ -0,0 +1,13 @@
+package cn.zingking.spring.easyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyServerApplication.class, args);
+    }
+
+}
